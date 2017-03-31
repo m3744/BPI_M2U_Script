06fe298e908eb893401ae1228706d441c6b9709c
mikeliu <mikeliu0216@hotmail.com>
2017-03-31 13:25:50 +0800

    modify fetch temp command

--- smartfans.sh
+++ smartfans.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-temp="$(/usr/sbin/smartctl -a /dev/sda |  grep Temperature_Celsius | awk '{print $4}')"
+temp="$(/usr/sbin/hddtemp /dev/sda  | /usr/bin/awk -F" " '{print substr($4, 1,2)}')"
 
 
-if [ ${temp} -gt 140 ] 
+if [ ${temp} -gt 40 ] 
 then
         #echo 277 > /sys/class/gpio/export
         #echo out > /sys/class/gpio/gpio277/direction	
 	echo 1 >/sys/class/gpio/gpio277/value
         echo "temp ${temp} enable fans"
-        sleep 180
+        sleep 280 
 else
 	#echo 277 > /sys/class/gpio/export 
         #echo out > /sys/class/gpio/gpio277/direction
